a222c95de71904bc81ba893b50e08cdbf450c42e
Lukasz Buturla <lukasz.buturla.25@gmail.com>
2022-04-28 17:25:41 +0200

    Add update function

--- Components/Clock/clock.py
+++ Components/Clock/clock.py
@@ -1,27 +1,58 @@
-from tkinter import Label
+from tkinter import *
 from Components.Style.style import gray
 from datetime import datetime
+from time import time, sleep
+from Components.Alarms.alarmManager import AlarmManager
 
 class Clock:
 
     window = None
+    hour = None
+    minute = None
 
     def __init__(self, root):
 
-        window = root
+        window = root       
+        
+        self.Load_time()
         self.ShowClock()
 
+    def Load_time(self):
+
+        print("1")
+
+        print(self.hour)
+        print(self.minute)
+
+        self.hour = datetime.today().hour
+        self.minute = datetime.today().minute
 
     def ShowClock(self):
 
-        print("tak")
+        print("2")
 
-        txt_01 = Label(self.window, text = "10:10", anchor="nw", font=("", 20), bg = gray)
+        print(self.hour)
+        print(self.minute)
+
+        strHour = str(self.hour)
+        strMinute = str(self.minute)
+
+        if len(strMinute) == 1:
+            self.minute = "0" + strMinute
+
+        txt_01 = Label(self.window, text = "{}:{}".format(self.hour, self.minute), anchor="nw", font=("Calibri", 20), bg = gray)
         txt_01.place(x = 276, y = 0)
+
 
     def Update(self):
 
-        pass
+        while True:
+            sleep(1 - time() % 1)
+
+            print("2")
+
+
+        
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from time import sleep
 from tkinter import *
 from Components.Ui.ui import MainView
 from Components.Clock.clock import Clock
 from Components.Style.style import gray, width, height
+from time import sleep, time
+
 
 class Main:
 
@@ -9,25 +12,39 @@ class Main:
     clock = None
 
     def __init__(self):
-
+        
         self.ConfigurateWindow()
-        clock = Clock(self.window)
+        self.clock = Clock(self.window)  
+        self.Update()
 
-        self.window.mainloop()
-
-    window = None
 
     def ConfigurateWindow(self):
-
+        
         self.window = Tk()
 
         self.window.geometry("{}x{}".format(width, height))
-        self.window.configure(bg = gray)   
+        self.window.configure(bg = gray)
+
+
+    def test(self):
+
+        self.window.configure(bg = "pink")
 
         
+    def Update(self):
 
+        while True:
+                        
+            sleep(0.1)          
 
-Main()
+            self.window.update()            
+            self.clock.Load_time()
+            self.clock.ShowClock()
+        
+
+if __name__ == "__main__":
+
+    Main()
 
 
 
